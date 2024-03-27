e8d696a6d43e71c8896b2d3ccf99be068e8fb70b
Jordan Acosta <jordanjacosta@gmail.com>
2024-03-27 11:06:40 -0700

    fix: remove backend config

--- versions.tf
+++ versions.tf
@@ -1,15 +1,12 @@
 terraform {
-  // TODO: uncomment when done testing
-  backend "s3" {}
-
   required_providers {
     azapi = {
-      source = "Azure/azapi"
+      source  = "Azure/azapi"
       version = "~> 1.12.1"
     }
 
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~> 3.94.0"
     }
   }

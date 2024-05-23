b45f8d4b5d0e079c2196d52877aae230a0957fef
Jordan Acosta <jordanjacosta@gmail.com>
2024-05-23 15:24:28 -0700

    refactor: break out network templates

---
+++ network/main.tf
@@ -0,0 +1,19 @@
+locals {
+  network = (
+    length(module.fully_managed_multiple_subnets_network) == 1 ?
+    module.fully_managed_multiple_subnets_network[0] :
+    module.fully_managed_single_subnet_network[0]
+  )
+}
+
+module "fully_managed_single_subnet_network" {
+  count               = var.template == "fully_managed_single_subnet" ? 1 : 0
+  source              = "./fully_managed_single_subnet"
+  resource_group_name = var.resource_group_name
+}
+
+module "fully_managed_multiple_subnets_network" {
+  count               = var.template == "fully_managed_multiple_subnets" ? 1 : 0
+  source              = "./fully_managed_multiple_subnets"
+  resource_group_name = var.resource_group_name
+}

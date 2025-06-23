09e6d72bba3eee888bf56783c27d0283b4aefc92
Jordan Acosta <jordanjacosta@gmail.com>
2025-06-23 14:17:30 -0700

    fix: temp name for rotation had to be moved

--- aks.tf
+++ aks.tf
@@ -46,20 +46,20 @@ module "aks" {
   # rbac_aad                                        = true
   # rbac_aad_managed                                = true
   # role_based_access_control_enabled = true
-  sku_tier                    = "Standard"
-  vnet_subnet                 = { id = data.azurerm_subnet.existing.id }
-  temporary_name_for_rotation = "${substr(var.nuon_id, 1, 7)}temp"
+  sku_tier    = "Standard"
+  vnet_subnet = { id = data.azurerm_subnet.existing.id }
   # attached_acr_id_map = {
   #   "${azurerm_container_registry.acr.name}" = azurerm_container_registry.acr.id
   # }
 
   node_pools = {
     "default" = {
-      name                  = "default"
-      vm_size               = var.vm_size
-      node_count            = var.node_count
-      vnet_subnet_id        = data.azurerm_subnet.existing.id
-      create_before_destroy = true
+      name                        = "default"
+      vm_size                     = var.vm_size
+      node_count                  = var.node_count
+      vnet_subnet_id              = data.azurerm_subnet.existing.id
+      create_before_destroy       = true
+      temporary_name_for_rotation = "${substr(var.nuon_id, 1, 7)}temp"
     }
   }
 }

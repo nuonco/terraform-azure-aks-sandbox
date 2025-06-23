7b6a6a2b1898c92ecd8672e0aff97947c343ed6a
Jordan Acosta <jordanjacosta@gmail.com>
2025-06-23 12:51:48 -0700

    fix: update aks rbac vars

--- aks.tf
+++ aks.tf
@@ -43,11 +43,11 @@ module "aks" {
   network_policy                                  = "azure"
   os_disk_size_gb                                 = 60
   private_cluster_enabled                         = false
-  rbac_aad                                        = true
+  # rbac_aad                                        = true
   # rbac_aad_managed                                = true
-  role_based_access_control_enabled = true
-  sku_tier                          = "Standard"
-  # vnet_subnet_id                    = data.azurerm_subnet.existing.id
+  # role_based_access_control_enabled = true
+  sku_tier                    = "Standard"
+  vnet_subnet                 = { id = data.azurerm_subnet.existing.id }
   temporary_name_for_rotation = "${substr(var.nuon_id, 1, 7)}temp"
   attached_acr_id_map = {
     "${azurerm_container_registry.acr.name}" = azurerm_container_registry.acr.id
